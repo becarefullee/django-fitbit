8114ca78f271ff664932480f0a6e1ebef32a0f0a
Max Willard <maxwell.rubin.willard@gmail.com>
2017-02-17 11:37:46 -0600

    Fixed migration from master to upstream changes.

--- fitapp/migrations/0001_initial.py
+++ fitapp/migrations/0001_initial.py
@@ -74,8 +74,26 @@ class Migration(migrations.Migration):
             field=models.ForeignKey(to=UserModel, help_text=b"The data's user"),
             preserve_default=True,
         ),
-        migrations.AlterUniqueTogether(
-            name='timeseriesdata',
-            unique_together=set([('user', 'resource_type', 'date')]),
-        ),
+        migrations.AddField(
+             model_name='timeseriesdata',
+             name='intraday',
+             field=models.BooleanField(default=False),
+             preserve_default=True,
+         ),
+         migrations.AlterField(
+             model_name='timeseriesdata',
+             name='date',
+             field=models.DateTimeField(),
+             preserve_default=True,
+         ),
+         migrations.AlterUniqueTogether(
+             name='timeseriesdata',
+             unique_together=set([('user', 'resource_type', 'date', 'intraday')]),
+         ),
+         migrations.AddField(
+             model_name='timeseriesdatatype',
+             name='intraday_support',
+             field=models.BooleanField(default=False),
+             preserve_default=True
+         )
     ]
--- fitapp/migrations/0003_auto_20151204_1438.py
+++ fitapp/migrations/0003_auto_20151204_1438.py
@@ -15,26 +15,4 @@ class Migration(migrations.Migration):
             name='timeseriesdata',
             options={'get_latest_by': 'date'},
         ),
-        migrations.AddField(
-            model_name='timeseriesdata',
-            name='intraday',
-            field=models.BooleanField(default=False),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='timeseriesdata',
-            name='date',
-            field=models.DateTimeField(),
-            preserve_default=True,
-        ),
-        migrations.AlterUniqueTogether(
-            name='timeseriesdata',
-            unique_together=set([('user', 'resource_type', 'date', 'intraday')]),
-        ),
-        migrations.AddField(
-            model_name='timeseriesdatatype',
-            name='intraday_support',
-            field=models.BooleanField(default=False),
-            preserve_default=True
-        )
     ]
