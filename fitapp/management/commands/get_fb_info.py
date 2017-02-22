1f2c67f2c7d4e25724a32196db8c521824567a64
Max Willard <maxwell.rubin.willard@gmail.com>
2017-02-22 13:43:03 -0600

    Fixed indentation.

--- fitapp/management/commands/get_fb_info.py
+++ fitapp/management/commands/get_fb_info.py
@@ -22,7 +22,7 @@ class Command(BaseCommand):
         """
 
     def add_arguments(self, parser):
-                parser.add_argument('username', type=str)
+        parser.add_argument('username', type=str)
 
     def handle(self, *args, **options):
         username = options['username']
