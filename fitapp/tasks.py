4f0dab54c90e4327bde6d8a3ea33260a65c1b936
madsilva <madeline-silva@uiowa.edu>
2017-10-16 17:04:50 -0500

    Fix int to string conversion bug in subscribe task.

--- fitapp/tasks.py
+++ fitapp/tasks.py
@@ -30,7 +30,7 @@ def subscribe(fitbit_user, subscriber_id):
     for fbuser in fbusers:
         fb = utils.create_fitbit(**fbuser.get_user_data())
         for collection in collections:
-            unique_id = fbuser.uuid + getattr(TimeSeriesDataType, collection)
+            unique_id = fbuser.uuid + str(getattr(TimeSeriesDataType, collection))
             try:
                 fb.subscription(unique_id, str(subscriber_id), collection=collection)
             except Exception as e:
